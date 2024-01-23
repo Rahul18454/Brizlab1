dfcd9f0926a1e79733531633daed515892f810c3
Rahul18454 <rahulgolukonda012@gmail.com>
2024-01-23 10:40:58 +0530

    second commit

---
+++ ifsta.sh
@@ -0,0 +1,7 @@
+read -p "Enter the Num1: " a
+read -p "enter the num2: " b
+if [ $a -gt $b ]
+then
+echo $a is greater then $b
+fi
+
---
+++ ifstatement.sh
@@ -0,0 +1,7 @@
+read -p "enter the num" a
+read -p "enter the num" b
+echo $a
+echo $b
+c=$(($a + $b))
+echo $c
+ 
